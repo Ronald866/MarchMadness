b8893364ff439332045933dd42bee61b9c7386b4
Crawford, Joseph (CORP) <Joseph.Crawford@adp.com>
2018-03-13 22:27:11 -0400

    update scraper

--- scraper.py
+++ scraper.py
@@ -3,31 +3,54 @@ import csv
 from bs4 import BeautifulSoup
 
 def main():
-    stats = get_stats_for_teams(["buffalo"])
+    teams = get_teams()
+    stats = get_stats_for_teams(teams)
+    headers = stats[0].keys()
     with open("stats.csv", "w") as csv_file:
-        writer = csv.DictWriter(csv_file, ["team","ppg", "opg", "fg_pct", "op_fg_pct", "tov", "op_tov"])
+        writer = csv.DictWriter(csv_file, headers)
         writer.writeheader()
         for team in stats:
             writer.writerow(team)
 
+def get_teams():
+    stat_page = "https://www.sports-reference.com/cbb/friv/forecast.cgi"
+    page = urllib2.urlopen(stat_page)
+    soup = BeautifulSoup(page, "html.parser")
+    table = soup.find("table", attrs={"id":"forecast"}).tbody
+    teamRows = table.find_all("tr", class_=lambda x: x != "thead")
+    teams = []
+    for t in teamRows:
+        teams.append(get_team_bracket_info(t))
+    return teams
+
+def get_team_bracket_info(teamRow):
+    team = {}
+    team["name"] = teamRow.find("a")["href"].split("/")[3]
+    team["region"] = teamRow.find("td", attrs={"data-stat":"region"}).text.strip()
+    team["seed"] = teamRow.find("td", attrs={"data-stat":"seed"}).text.strip()
+    return team
+
 def get_stats_for_teams(teams):
     team_stats = []
     for team in teams:
-        tbody_soup = get_tbody_soup(team)
+        tbody_soup = get_tbody_soup(team["name"])
         stats = get_stats(tbody_soup)
-        stats["team"] = team
-        team_stats.append(stats)
+        team.update(stats)
+        team_stats.append(team)
     return team_stats
 
 def get_tbody_soup(team):
-    stat_page = "https://www.sports-reference.com/cbb/schools/{}/2018.html".format(team)
-    page = urllib2.urlopen(stat_page)
-    soup = BeautifulSoup(page, "html.parser")
-    return soup.find("table", attrs={"id":"team_stats"}).tbody
+    try:
+        stat_page = "https://www.sports-reference.com/cbb/schools/{}/2018.html".format(team)
+        page = urllib2.urlopen(stat_page)
+        soup = BeautifulSoup(page, "html.parser")
+        return soup.find("table", attrs={"id":"team_stats"}).tbody
+    except urllib2.HTTPError, e:
+        print team + " could not be found"
 
 def get_stats(tbody):
-    print(tbody)
     stats = {}
+    stats["gs"] = get_stat(tbody, "g")
     stats["ppg"] = get_stat(tbody, "pts_per_g")
     stats["opg"] = get_stat(tbody, "pts_per_g", True)
     stats["fg_pct"] = get_stat(tbody, "fg_pct")
